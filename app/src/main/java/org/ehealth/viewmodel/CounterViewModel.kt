cb74d1229b7ac62b296f93324c65fb86d832e810
fdankwah <fredrick@appsnmobilesolutions.com>
2024-09-15 23:37:51 +0000

    new commit

--- app/src/main/java/org/ehealth/viewmodel/CounterViewModel.kt
+++ app/src/main/java/org/ehealth/viewmodel/CounterViewModel.kt
@@ -2,23 +2,30 @@ package org.ehealth.viewmodel
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-data class  CounterViewModel(
-    var _count: Int = 0,
+data class CounterViewModel(
+    var count: Int = 0,
 )
-
-
 
 @Composable
 fun CounterApp() {
+    // Remember and hold the state of the counter across recompositions
+    var ct by remember { mutableStateOf(0) }
+
     Column(
         modifier = Modifier
             .fillMaxHeight()
@@ -26,18 +33,39 @@ fun CounterApp() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-
-        Text(text = "Count: ")
+        Text(text = "Count: $ct")
+        Spacer(modifier = Modifier.padding(10.dp))
+        Row {
+            Increment(ct.toString()) // Pass the counter state to Increment composable
+            Spacer(modifier = Modifier.padding(20.dp))
+            Decrement(ct.toString()) // Pass the counter state to Decrement composable
+        }
     }
 }
 
 @Composable
-fun increment() {
-//    increment of the count
+fun Increment(ct: String) {
+    var count = ct.toInt();
+    Button(
+        onClick = {
+            count += 1;
+            println(count);
+            print(count);
+            println("hello world")
+        },
+    ) {
+        Text(text = "Increment")
+    }
 }
 
-
 @Composable
-fun decrement() {
-//    decrease the count
+fun Decrement(ct: String) {
+    var count = ct.toInt()
+    Button(
+        onClick = {
+            count -= 1; // Decrement the shared count
+        },
+    ) {
+        Text(text = "Decrement")
+    }
 }
